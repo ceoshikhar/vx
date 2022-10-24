e6e1678ccbbe1b08036404bfbcc4b1e5b3873370
Shikhar Sharma <ceoshikhar@gmail.com>
2022-10-24 18:33:32 +0530

    chore: remove log

--- vx_test.go
+++ vx_test.go
@@ -1,7 +1,6 @@
 package vx
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -12,8 +11,6 @@ func TestSimpleString(t *testing.T) {
 
 	v := simpleString{a: "as"}
 	ok, errors := ValidateStruct(v)
-
-	fmt.Println(ok, errors)
 
 	if !ok {
 		for _, err := range errors {
