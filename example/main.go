a0a85c5d9c155a0ac74bc74e02dae90d615c8b6c
Shikhar Sharma <ceoshikhar@gmail.com>
2022-11-01 01:00:04 +0530

    feat(example/main.go): return response in JSON

--- example/main.go
+++ example/main.go
@@ -11,11 +11,14 @@ type AssocFilter map[string]interface{}
 
 type BonusFilter map[string]interface{}
 
+type AssocOrBonusFilter map[string]interface{}
+
 type user struct {
-	Name  string      `vx:"name=name, required"`
-	Age   interface{} `vx:"name=age, type=int, required"`
-	Assoc AssocFilter
-	Bonus BonusFilter
+	Name                     any `vx:"name=name, type=string, required, minLength=3"`
+	Age                      any `vx:"name=age, type=float64, required"`
+	Location                 any `vx:"name=location, type=[]string"`
+	AssocOrBonus             any `vx:"type=map[string]string"`
+	HowWeWannaDoAssocOrBonus AssocOrBonusFilter
 }
 
 func test(w http.ResponseWriter, req *http.Request) {
@@ -28,18 +31,43 @@ func test(w http.ResponseWriter, req *http.Request) {
 	}
 
 	res, ok := vx.ValidateStruct(u)
+	fmt.Println("ok:", ok)
+	fmt.Println("res:", res)
+
+	w.Header().Set("Content-Type", "application/json")
+	var jsonRes []byte = []byte{}
 
 	if !ok {
-		http.Error(w, res.String(), http.StatusInternalServerError)
+		jsonRes, err = json.Marshal(res.StringArray())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(jsonRes)
 		return
 	}
 
 	if len(res.Errors) > 0 {
-		http.Error(w, res.String(), http.StatusBadRequest)
+		jsonRes, err = json.Marshal(res.StringArray())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(jsonRes)
 		return
 	}
 
-	fmt.Fprintf(w, "User: %+v", u)
+	// fmt.Fprintf(w, "User: %+v", u)
+
+	jsonRes, err = json.Marshal(u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonRes)
 }
 
 func main() {
